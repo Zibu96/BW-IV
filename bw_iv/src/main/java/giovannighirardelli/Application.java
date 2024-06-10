e7b2f50f2845f39e95b6a81c8488d4c1d34651d4
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-06-10 12:01:20 +0200

    Initial commit

---
+++ bw_iv/src/main/java/giovannighirardelli/Application.java
@@ -0,0 +1,14 @@
+package giovannighirardelli;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporti");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+    }
+}
---
+++ bw_iv/src/test/java/giovannighirardelli/ApplicationTest.java
@@ -0,0 +1,13 @@
+package giovannighirardelli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
