f253962c8b511e93e3b67777fe4711a84d96a0b1
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-11 11:22:26 +0200

    tratta

--- bw_iv/src/main/java/giovannighirardelli/entities/PublicTransport.java
+++ bw_iv/src/main/java/giovannighirardelli/entities/PublicTransport.java
@@ -22,4 +22,68 @@ public class PublicTransport {
     private StatePublicTrasport statePublicTrasport;
 @OneToMany (mappedBy = "publicTransport")
     private List<Ticket> tickets;
+@ManyToMany (mappedBy = "publicTransports")
+    private List<Route> routes;
+public PublicTransport(){}
+    public PublicTransport(TypePublicTrasport typePublicTrasport, int max_capacity, StatePublicTrasport statePublicTrasport) {
+        this.typePublicTrasport = typePublicTrasport;
+        this.max_capacity = max_capacity;
+        this.statePublicTrasport = statePublicTrasport;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+
+
+    public TypePublicTrasport getTypePublicTrasport() {
+        return typePublicTrasport;
+    }
+
+    public void setTypePublicTrasport(TypePublicTrasport typePublicTrasport) {
+        this.typePublicTrasport = typePublicTrasport;
+    }
+
+    public int getMax_capacity() {
+        return max_capacity;
+    }
+
+    public void setMax_capacity(int max_capacity) {
+        this.max_capacity = max_capacity;
+    }
+
+    public StatePublicTrasport getStatePublicTrasport() {
+        return statePublicTrasport;
+    }
+
+    public void setStatePublicTrasport(StatePublicTrasport statePublicTrasport) {
+        this.statePublicTrasport = statePublicTrasport;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public List<Route> getRoutes() {
+        return routes;
+    }
+
+    public void setRoutes(List<Route> routes) {
+        this.routes = routes;
+    }
+
+    @Override
+    public String toString() {
+        return "PublicTransport{" +
+                "statePublicTrasport=" + statePublicTrasport +
+                ", max_capacity=" + max_capacity +
+                ", typePublicTrasport=" + typePublicTrasport +
+                ", id=" + id +
+                '}';
+    }
 }
