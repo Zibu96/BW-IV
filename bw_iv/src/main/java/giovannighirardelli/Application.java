bdd9558eb651881b198c6aca65bc27ebfe5ab932
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-06-14 10:52:38 +0200

    first scanner buy a Ticket

--- bw_iv/src/main/java/giovannighirardelli/Application.java
+++ bw_iv/src/main/java/giovannighirardelli/Application.java
@@ -1,103 +1,18 @@
 package giovannighirardelli;
 
-import giovannighirardelli.dao.*;
-import giovannighirardelli.entities.*;
-import giovannighirardelli.enums.StatePublicTransport;
-import giovannighirardelli.enums.TaskStatus;
-import giovannighirardelli.enums.TicketType;
-import giovannighirardelli.enums.TypePublicTrasport;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
+
+import static giovannighirardelli.utilities.MyScanner.myScanner;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporti");
 
     public static void main(String[] args) {
 
-
-        EntityManager em = emf.createEntityManager();
-        TicketOfficeDao tod = new TicketOfficeDao(em);
-        CardDao cd = new CardDao(em);
-        UserDao ud = new UserDao(em);
-        PublicTransportDao ptd = new PublicTransportDao(em);
-        RouteDao rd = new RouteDao(em);
-        RetailerDao rld = new RetailerDao(em);
-        OnDutyDao odd = new OnDutyDao(em);
-        MaintanceDao md = new MaintanceDao(em);
-        QueriesUtilitiesDao qd = new QueriesUtilitiesDao(em);
-        TransportRouteDao trd = new TransportRouteDao(em);
-
-        User user = new User("Christian", "Martucci", LocalDate.of(2001, 9, 11));
-        //  ud.userSave(user);
-
-        Card card = new Card(LocalDate.now(), user);
-        // cd.cardSave(card);
-
-
-        Ticket ticket = new Ticket("Tiburtina", LocalDate.now(), rld.findById("1bf261a4-8349-49a4-8da1-bb06c57dd1cc"), TicketType.LONG_DISTANCE);
-//        tod.ticketOfficeSave(ticket);
-
-        // Subscription sub = new Subscription("Termini", LocalDate.now(), SubscriptionType.WEEKLY, cd.findById("924ce772-91d3-4d3b-948c-e640b35454a8"));
-        // tod.ticketOfficeSave(sub);
-
-        PublicTransport pubTr = new PublicTransport(TypePublicTrasport.TRAM, 81, StatePublicTransport.MAINTENANCE);
-//        ptd.publicTransportSave(pubTr);
-
-        Route route = new Route("Tiburtina", "Termini", 2.5);
-//        rd.routeSave(route);
-
-        Maintenance main = new Maintenance("Guasto motore", LocalDate.now().minusMonths(8), LocalDate.now().minusMonths(7), ptd.findById("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d"));
-//        md.maintananceSave(main);
-
-        OnDuty onDuty = new OnDuty(LocalDate.now().minusMonths(2), LocalDate.now(), 7.2, ptd.findById("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d"));
-//        odd.ondDutySave(onDuty);
-
-        VendingMachine vdMachine = new VendingMachine("Tiburtina", TaskStatus.ACTIVE);
-        // rld.retailerSave(vdMachine);
-
-        Authorised aut = new Authorised("Termini", "Tabacchi", 3568213L);
-        // rld.retailerSave(aut);
-
-//        Retailer retailer = rld.findById("492269d4-1d3e-428e-a497-71c996f21e89");
-//        vdMachine.setTicketOffice(tod.findById("3ada0c08-e436-4c9b-a2f0-8ee8d7ad7474"));
-//        rld.retailerSave(vdMachine);
-
-//       qd.findAllTicketOfficeFromVendingMachine(LocalDate.now(), "Termini");
-//        qd.findAllTicketOfficeFromAuthorised(LocalDate.now(), "Termini");
-
-//        qd.findAllTicketsFromVendingMachine(LocalDate.now().minusDays(1), LocalDate.now().plusDays(1)).forEach(System.out::println);
-
-
-        // qd.findAllTicketFromRetailer(LocalDate.now().minusDays(2), LocalDate.now().plusDays(2), retailer.getId().toString());
-
-        // qd.subscriptionValidationCheck("924ce772-91d3-4d3b-948c-e640b35454a8");
-
-//        try {
-//            rld.setStatusOfVendingMachine(TaskStatus.ACTIVE, "492269d4-1d3e-428e-a497-71c996f21e89");
-//        } catch (NotFoundException err) {
-//            System.out.println(err.getMessage());
-//        }
-//
-//        ptd.findAllPublicTransportByStatus(StatePublicTransport.MAINTENANCE);
-//
-//        ptd.getAPublicTransportStatus("e5e57aec-1b8d-4a1d-a089-335fcda54a57");
-
-
-//        ptd.getBacklogOfTransportStatus("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d");
-//        ptd.findAllPublicTransportByStatus(StatePublicTransport.MAINTENANCE);
-//        ptd.getAPublicTransportStatus("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d");
-
-        TransportRoute transportRoute = new TransportRoute(ptd.findById("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d"), rd.findById("4e1c8dac-c049-4305-9f87-f9a4bf45a930"), 2.8);
-//        trd.transportRouteSave(transportRoute);
-
-        trd.findRouteByTransport("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d", "4e1c8dac-c049-4305-9f87-f9a4bf45a930");
-//        tod.validateTicket("2bb427bc-8f9c-493e-8ff9-821a6022c3aa", "0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d");
-//        tod.getTicketByTransport("0f0fd584-1a4c-44fa-8c64-2e08c2c83d0d");
-
-//        tod.getTicketByDate(LocalDate.now().minusDays(2), LocalDate.now().plusDays(2));
+        myScanner();
     }
 
 }
--- bw_iv/src/main/java/giovannighirardelli/utilities/MyScanner.java
+++ bw_iv/src/main/java/giovannighirardelli/utilities/MyScanner.java
@@ -1,10 +1,35 @@
 package giovannighirardelli.utilities;
 
+import giovannighirardelli.dao.*;
+import giovannighirardelli.entities.Subscription;
+import giovannighirardelli.entities.Ticket;
+import giovannighirardelli.enums.SubscriptionType;
+import giovannighirardelli.enums.TicketType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class MyScanner {
 
-    public static void main(String[] args) {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporti");
+
+    public static void myScanner() {
+        EntityManager em = emf.createEntityManager();
+        TicketOfficeDao tod = new TicketOfficeDao(em);
+        CardDao cd = new CardDao(em);
+        UserDao ud = new UserDao(em);
+        PublicTransportDao ptd = new PublicTransportDao(em);
+        RouteDao rd = new RouteDao(em);
+        RetailerDao rld = new RetailerDao(em);
+        OnDutyDao odd = new OnDutyDao(em);
+        MaintanceDao md = new MaintanceDao(em);
+        QueriesUtilitiesDao qd = new QueriesUtilitiesDao(em);
+        TransportRouteDao trd = new TransportRouteDao(em);
+
+
         int el = 0;
         Scanner scn = new Scanner(System.in);
         while (el != 3) {
@@ -42,13 +67,164 @@ public class MyScanner {
                                     el2 = Integer.parseInt(scn.nextLine());
                                     switch (el2){
                                         case 1:{
-                                            
+                                            int el3 = 0;
+                                            while (el3 !=3) {
+                                                System.out.println("Vuoi acquistare dal rivenditore autorizzato o dal distributore automatico?");
+                                                System.out.println("Inserisci 1 per selezionare il rivenditore autorizzato.");
+                                                System.out.println("Inserisci 2 per selezionare il distributore automatico");
+                                                System.out.println("inserisci 3 per tornare indietro");
+                                                el3 = Integer.parseInt(scn.nextLine());
+
+                                                switch (el3) {
+                                                    case 1:
+                                                    {
+                                                        int el4 =0;
+                                                        while (el4 != 4) {
+                                                            System.out.println("Che tipo di biglietto vuoi acquistare?");
+                                                            System.out.println("Inserisci 1 per selezionare il biglietto ordinario (1,50 €).");
+                                                            System.out.println("Inserisci 2 per selezionare il biglietto a breve distanza 15 km (2,00 €)");
+                                                            System.out.println("Inserisci 3 per selezionare il biglietto a lunga distanza 30 km (3,00 €)");
+                                                            System.out.println("inserisci 4 per tornare indietro");
+                                                            el4 = Integer.parseInt(scn.nextLine());
+
+                                                            ticketTypeFunctionSel(el4, "Tiburtina", "2419fa42-0b92-44ac-a949-422752214d48" );
+
+                                                        }
+
+                                                        break;
+                                                    }
+
+                                                    case 2: {
+                                                        int el5 =0;
+                                                        while (el5 != 4) {
+                                                            System.out.println("Che tipo di biglietto vuoi acquistare?");
+                                                            System.out.println("Inserisci 1 per selezionare il biglietto ordinario (1,50 €).");
+                                                            System.out.println("Inserisci 2 per selezionare il biglietto a breve distanza 15 km (2,00 €)");
+                                                            System.out.println("Inserisci 3 per selezionare il biglietto a lunga distanza 30 km (3,00 €)");
+                                                            System.out.println("inserisci 4 per tornare indietro");
+                                                            el5 = Integer.parseInt(scn.nextLine());
+
+                                                            ticketTypeFunctionSel(el5, "Tiburtina", "492269d4-1d3e-428e-a497-71c996f21e89" );
+
+                                                        }
+                                                        break;
+                                                    }
+
+                                                    case 3: break;
+
+                                                    default:
+                                                        System.out.println("Dato inserito non valido");
+                                                }
+                                            }
                                             break;
                                         }
-                                        case 2:{
+                                        case 2:
+                                        {
+                                            int el6 = 0;
+                                            while (el6 !=3) {
+                                                System.out.println("Vuoi acquistare dal rivenditore autorizzato o dal distributore automatico?");
+                                                System.out.println("Inserisci 1 per selezionare il rivenditore autorizzato.");
+                                                System.out.println("Inserisci 2 per selezionare il distributore automatico");
+                                                System.out.println("inserisci 3 per tornare indietro");
+                                                el6 = Integer.parseInt(scn.nextLine());
+
+                                                switch (el6) {
+                                                    case 1:
+                                                    {
+                                                        int el7 =0;
+                                                        while (el7 != 4) {
+                                                            System.out.println("Che tipo di biglietto vuoi acquistare?");
+                                                            System.out.println("Inserisci 1 per selezionare il biglietto ordinario (1,50 €).");
+                                                            System.out.println("Inserisci 2 per selezionare il biglietto a breve distanza 15 km (2,00 €)");
+                                                            System.out.println("Inserisci 3 per selezionare il biglietto a lunga distanza 30 km (3,00 €)");
+                                                            System.out.println("inserisci 4 per tornare indietro");
+                                                            el7 = Integer.parseInt(scn.nextLine());
+
+                                                            ticketTypeFunctionSel(el7,  "Termini", "676cdca1-4bbc-4a82-9640-01499391caa5" );
+
+                                                        }
+
+                                                        break;
+                                                    }
+
+                                                    case 2: {
+                                                        int el8 =0;
+                                                        while (el8 != 4) {
+                                                            System.out.println("Che tipo di biglietto vuoi acquistare?");
+                                                            System.out.println("Inserisci 1 per selezionare il biglietto ordinario (1,50 €).");
+                                                            System.out.println("Inserisci 2 per selezionare il biglietto a breve distanza 15 km (2,00 €)");
+                                                            System.out.println("Inserisci 3 per selezionare il biglietto a lunga distanza 30 km (3,00 €)");
+                                                            System.out.println("inserisci 4 per tornare indietro");
+                                                            el8 = Integer.parseInt(scn.nextLine());
+
+                                                            ticketTypeFunctionSel(el8, "Termini", "c7bb2c28-de41-48e9-bc1c-00d7fe06f01b" );
+
+                                                        }
+                                                        break;
+                                                    }
+
+                                                    case 3: break;
+
+                                                    default:
+                                                        System.out.println("Dato inserito non valido");
+                                                }
+                                            }
+
                                             break;
                                         }
-                                        case 3:{
+                                        case 3:
+                                        {
+                                            int el9 = 0;
+                                            while (el9 !=3) {
+                                                System.out.println("Vuoi acquistare dal rivenditore autorizzato o dal distributore automatico?");
+                                                System.out.println("Inserisci 1 per selezionare il rivenditore autorizzato.");
+                                                System.out.println("Inserisci 2 per selezionare il distributore automatico");
+                                                System.out.println("inserisci 3 per tornare indietro");
+                                                el9= Integer.parseInt(scn.nextLine());
+
+                                                switch (el9) {
+                                                    case 1:
+                                                    {
+                                                        int el10 =0;
+                                                        while (el10 != 4) {
+
+                                                            System.out.println("Che tipo di biglietto vuoi acquistare?");
+                                                            System.out.println("Inserisci 1 per selezionare il biglietto ordinario (1,50 €).");
+                                                            System.out.println("Inserisci 2 per selezionare il biglietto a breve distanza 15 km (2,00 €)");
+                                                            System.out.println("Inserisci 3 per selezionare il biglietto a lunga distanza 30 km (3,00 €)");
+                                                            System.out.println("inserisci 4 per tornare indietro");
+                                                            el10 = Integer.parseInt(scn.nextLine());
+
+                                                            ticketTypeFunctionSel(el10, "Colosseo", "05a2e1dc-653a-42f5-8e55-0cf50427a38d" );
+
+                                                        }
+
+                                                        break;
+                                                    }
+
+                                                    case 2: {
+                                                        int el11 =0;
+                                                        while (el11 != 4) {
+
+                                                            System.out.println("Che tipo di biglietto vuoi acquistare?");
+                                                            System.out.println("Inserisci 1 per selezionare il biglietto ordinario (1,50 €).");
+                                                            System.out.println("Inserisci 2 per selezionare il biglietto a breve distanza 15 km (2,00 €)");
+                                                            System.out.println("Inserisci 3 per selezionare il biglietto a lunga distanza 30 km (3,00 €)");
+                                                            System.out.println("inserisci 4 per tornare indietro");
+                                                            el11 = Integer.parseInt(scn.nextLine());
+
+                                                            ticketTypeFunctionSel(el11,  "Colosseo", "fe235d37-a3fa-4c6b-966e-47ac76b8c2ea" );
+
+                                                        }
+                                                        break;
+                                                    }
+
+                                                    case 3: break;
+
+                                                    default:
+                                                        System.out.println("Dato inserito non valido");
+                                                }
+                                            }
                                             break;
                                         }
                                         case 4: break;
@@ -84,5 +260,72 @@ public class MyScanner {
         }
     }
 
+    private static void subTypeFunctionSel (int el, Scanner scn, String location, String idRetailer, String idCard) {
+        EntityManager em = emf.createEntityManager();
+        TicketOfficeDao tod = new TicketOfficeDao(em);
+        RetailerDao rld = new RetailerDao(em);
+        CardDao cd = new CardDao(em);
+
+        System.out.println("Che tipo di abbonamento vuoi acquistare?");
+        System.out.println("Inserisci 1 per selezionare l'abbonamento settimanale (15.99 €).");
+        System.out.println("Inserisci 2 per selezionare l'abbonamento mensile (30.59 €).");
+        System.out.println("inserisci 3 per tornare indietro");
+        el = Integer.parseInt(scn.nextLine());
+
+        switch (el) {
+            case 1:
+            {
+                Subscription subscription = new Subscription(location, LocalDate.now(),  rld.findById(idRetailer), SubscriptionType.WEEKLY, cd.findById(idCard));
+                System.out.println("Grazie per l'acquisto!");
+                break;
+            }
+
+            case 2: {
+                Subscription subscription = new Subscription(location, LocalDate.now(),  rld.findById(idRetailer), SubscriptionType.MONTLY, cd.findById(idCard));
+                System.out.println("Grazie per l'acquisto!");
+                break;
+            }
+
+            case 3: break;
+
+            default:
+                System.out.println("Dato inserito non valido");
+        }
+    }
+
+    private static void ticketTypeFunctionSel(int el, String location, String id ) {
+
+        EntityManager em = emf.createEntityManager();
+        TicketOfficeDao tod = new TicketOfficeDao(em);
+        RetailerDao rld = new RetailerDao(em);
+
+        switch (el ) {
+            case 1: {
+                Ticket ticket = new Ticket(location, LocalDate.now(), rld.findById(id), TicketType.ORDINARY);
+                tod.ticketOfficeSave(ticket);
+                System.out.println("Grazie per l'acquisto!");
+                break;
+            }
+
+            case 2: {
+                Ticket ticket = new Ticket(location, LocalDate.now(), rld.findById(id), TicketType.SHORT_DISTANCE);
+                tod.ticketOfficeSave(ticket);
+                System.out.println("Grazie per l'acquisto!");
+                break;
+            }
+
+            case 3: {
+                Ticket ticket = new Ticket(location, LocalDate.now(), rld.findById(id), TicketType.LONG_DISTANCE);
+                tod.ticketOfficeSave(ticket);
+                System.out.println("Grazie per l'acquisto!");
+                break;
+            }
+
+            case 4: break;
+
+            default:
+                System.out.println("Dato inserito non valido");
+        }
+    }
 
 }
